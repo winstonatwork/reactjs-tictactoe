db5922eb5bdfa0f2f7e35dc924ae307b3d520e75
eric-hunter <integratedchaos1@gmail.com>
2019-06-18 15:39:36 -0400

    *sigh* moved most of everything into game component, in order to keep track of game history more appropriately.

--- reactjs-tictactoe/src/index.js
+++ reactjs-tictactoe/src/index.js
@@ -13,50 +13,28 @@ function Square(props) {
 
 class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
+    //constructor(props) {
+    //    super(props);
 
-        this.state = {
-            squares: Array(9).fill(null),
-            //default, X goes first.
-            xIsNext: true,
-            winner: null
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.state.winner || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-            winner: calculateWinner(squares)
-        });
-    }
+    //    this.state = {
+    //        squares: Array(9).fill(null),
+    //        //default, X goes first.
+    //        xIsNext: true,
+    //        winner: null
+    //    };
+    //}
 
     renderSquare(i) {
         return (<Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        let status;
-        if (this.state.winner) {
-            status = 'Winner: ' + this.state.winner;
-        }
-        else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -78,14 +56,64 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            xIsNext: true,
+            winner: null
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (this.state.winner || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            xIsNext: !this.state.xIsNext,
+            winner: calculateWinner(squares)
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+
+        this.state.winner = calculateWinner(current.squares);
+
+        let status;
+        if (this.state.winner) {
+            status = 'Winner: ' + this.state.winner;
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
